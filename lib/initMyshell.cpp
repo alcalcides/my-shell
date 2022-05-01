2845e236647a4c20688664609a2faadfbec22e65
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-04-30 22:31:42 -0300

    Reads the first word user command

--- lib/initMyshell.cpp
+++ lib/initMyshell.cpp
@@ -1,9 +1,17 @@
 #include "./initMyshell.hpp"
 
 #include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LENGH_STRING 10
 
 using namespace std;
 
 void initMyshell() {
+    string input;
+
     cout << "myshell> ";
+    cin >> input;
+    cout << "myshell: user's command: " << input << endl;
 }

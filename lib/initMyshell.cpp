e022af34504bda30c9975069134142544e40763d
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-05-08 00:51:27 -0300

    Iterates until enter 'exit' or 'quit'

--- lib/initMyshell.cpp
+++ lib/initMyshell.cpp
@@ -4,12 +4,16 @@
 #include "./execute.hpp"
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define MAX_STRING_LENGH 4096
 #define MAX_NUMBER_OF_WORDS 100
 
 void initMyshell()
 {
+
+init:
 	char input[MAX_STRING_LENGH];
 	char *word[MAX_NUMBER_OF_WORDS];
 	int wordCounter = 0;
@@ -26,4 +30,9 @@ void initMyshell()
 	// seeArgs(word, wordCounter);
 
 	execute(word);
+	
+	if (strcmp("quit", word[0]) != 0 && strcmp("exit", word[0]) != 0)
+		goto init;
+
+	exit(EXIT_SUCCESS);
 }

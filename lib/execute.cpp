92111de9e88a547cb1f185c239a27e70c58fc54a
Alcides Augusto Bezerra Neto <alcalcides@hotmail.com>
2022-05-10 16:08:25 -0300

    Tracks child processes events

--- lib/execute.cpp
+++ lib/execute.cpp
@@ -37,7 +37,32 @@ bool shouldRepeat(char *cmd)
 void playParent(pid_t child_pid)
 {
     printProcessStarted(child_pid);
-    wait(0);
+    int status;
+    pid_t wpid;
+
+    wpid = waitpid(child_pid, &status, WUNTRACED | WCONTINUED);
+    if (wpid == -1)
+    {
+        printError("waitpid failure");
+        exit(EXIT_FAILURE);
+    }
+
+    if (WIFEXITED(status)) // SIGTERM(15)
+    {
+        printProcessExited(child_pid);
+    }
+    else if (WIFSIGNALED(status)) // SIGKILL(9)
+    {
+        printProcessKilled(child_pid);
+    }
+    else if (WIFSTOPPED(status)) // SIGTSTP(20)
+    {
+        printProcessStopped(child_pid);
+    }
+    else if (WIFCONTINUED(status)) // SIGCONT(18)
+    {
+        printProcessContinued(child_pid);
+    }
 }
 
 void playChild(char **word)
